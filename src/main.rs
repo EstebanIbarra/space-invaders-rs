4c0a51e7f61009223d91fc8f92d53c8485836bf2
Esteban Ibarra <sectorjeat@gmail.com>
2024-01-20 14:39:35 -0600

    Implements game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event;
+use crossterm::event::Event;
+use space_invaders::game::Game;
+use std::io::Result;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() -> Result<()> {
+    let mut game = Game::new()?;
+    loop {
+        if event::poll(Duration::from_millis(100))? {
+            let key = event::read()?;
+            if let Event::Key(key) = key {
+                game.handle_input(key);
+            }
+        }
+        game.update();
+        game.draw();
+        sleep(Duration::from_millis(16));
+    }
 }
