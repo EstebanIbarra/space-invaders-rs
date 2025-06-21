ee41d14720efb75e277e90fe24c392595de9fbdb
EstebanIbarra <sectorjeat@gmail.com>
2025-06-21 12:34:55 -0600

    Removes update method as is not necessary, refactors From implementation and UnicodeMatrix type alias

--- src/sprite/mod.rs
+++ src/sprite/mod.rs
@@ -1,7 +1,9 @@
 #[cfg(test)]
 mod tests;
 
-pub type UnicodeMatrix = Vec<Vec<u32>>;
+use std::iter::repeat_n;
+
+pub type UnicodeMatrix = [[u32; 4]; 2];
 pub type CharMatrix = Vec<Vec<char>>;
 
 pub struct Sprite {
@@ -13,37 +15,35 @@ pub struct Sprite {
 
 impl From<(UnicodeMatrix, f32)> for Sprite {
     fn from((matrix, scale): (UnicodeMatrix, f32)) -> Sprite {
-        let char_matrix: CharMatrix = matrix
-            .into_iter()
-            .map(|row| {
-                row.into_iter()
-                    .map(|cp| char::from_u32(cp).unwrap_or('\u{FFFD}'))
-                    .collect()
-            })
-            .collect();
-        let og_height = char_matrix.len();
-        let og_width = char_matrix.first().map_or(0, |r| r.len());
-        let height = (((og_height as f32) * scale).max(1.0).round() as usize).min(og_height);
-        let width = (((og_width as f32) * scale).max(1.0).round() as usize).min(og_width);
-        let data = char_matrix
-            .iter()
-            .take(height)
-            .map(|row| row.iter().cloned().take(width).collect::<Vec<_>>())
-            .collect();
+        let char_rows: [[char; 4]; 2] = matrix.map(|row| {
+            let mut arr = ['\u{FFFD}'; 4];
+            for (i, &cp) in row.iter().enumerate() {
+                arr[i] = char::from_u32(cp).unwrap_or('\u{FFFD}');
+            }
+            arr
+        });
+        let mut width = (3.0 * scale).round().max(3.0) as usize;
+        if width % 2 == 0 {
+            width += 1;
+        }
+        let fill_count = (width - 3) / 2;
+        let mut data: CharMatrix = Vec::with_capacity(2);
+        for row in &char_rows {
+            let filler = row[3];
+            let mut new_row = Vec::with_capacity(width);
+            new_row.push(row[0]);
+            new_row.extend(repeat_n(filler, fill_count));
+            new_row.push(row[1]);
+            new_row.extend(repeat_n(filler, fill_count));
+            new_row.push(row[2]);
+            data.push(new_row);
+        }
+
         Sprite {
             width,
-            height,
+            height: 2,
             scale,
             data,
         }
-    }
-}
-
-impl Sprite {
-    pub fn update(&mut self, matrix: UnicodeMatrix) {
-        let new: Sprite = (matrix, self.scale).into();
-        self.width = new.width;
-        self.height = new.height;
-        self.data = new.data;
     }
 }
