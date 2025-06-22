3c933797c55c0bb37596f4486a90c78d58e3d286
EstebanIbarra <sectorjeat@gmail.com>
2025-06-22 16:24:51 -0600

    Adds SetSprite trait, NO_SPRITE sprite and DifficultyLevel enum

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -13,11 +13,34 @@ use player::Player;
 use projectile::Projectile;
 
 pub type Position = (usize, usize);
+pub type Sprite = [[char; 3]; 2];
+
+pub trait SetSprite {
+    fn set_sprite(&mut self);
+}
+
+pub const NO_SPRITE: Sprite = [
+    ['\u{0020}', '\u{0020}', '\u{0020}'],
+    ['\u{0020}', '\u{0020}', '\u{0020}'],
+];
+
+#[derive(Debug, PartialEq)]
+pub enum Owner {
+    Player,
+    Invader,
+}
 
 pub enum State {
     Start,
     Playing,
     GameOver,
+}
+
+pub enum DifficultyLevel {
+    Easy,
+    Normal,
+    Hard,
+    Nightmare,
 }
 
 pub struct Screen {
@@ -27,12 +50,13 @@ pub struct Screen {
 
 pub struct Game<'a> {
     pub state: State,
+    pub difficulty: DifficultyLevel,
     pub score: usize,
     pub level: u16,
     pub tick: usize,
     pub screen: &'a mut Screen,
-    pub player: &'a mut Player<'a>,
-    pub invaders: Vec<&'a mut Invader<'a>>,
-    pub projectiles: Vec<&'a mut Projectile<'a>>,
-    pub obstacles: Vec<&'a mut Obstacle<'a>>,
+    pub player: &'a mut Player,
+    pub invaders: Vec<&'a mut Invader>,
+    pub projectiles: Vec<&'a mut Projectile>,
+    pub obstacles: Vec<&'a mut Obstacle>,
 }
