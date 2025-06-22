0ffbf0d05d048274824839deca7693015f605c4c
EstebanIbarra <sectorjeat@gmail.com>
2025-06-22 16:30:56 -0600

    Adds implementations for Shield and sprites

--- src/game/shield/mod.rs
+++ src/game/shield/mod.rs
@@ -1,52 +1,123 @@
 #[cfg(test)]
 mod tests;
 
-use super::Position;
-use crate::sprite::{Sprite, UnicodeMatrix};
+use super::{Owner, Position, SetSprite, Sprite, NO_SPRITE};
 
-pub struct Shield<'a> {
-    pub position: Position,
-    pub active: bool,
-    pub health: u8,
-    pub sprite: &'a mut Sprite,
+#[derive(Clone, Debug, PartialEq)]
+pub enum ShieldType {
+    None,
+    Single,
+    Double,
+    Triple,
+    Quad,
 }
 
-pub const P_S_SHIELD: UnicodeMatrix = [
-    [0x20, 0x20, 0x20, 0x20],         //     Filler:
-    [0x250C, 0x2500, 0x2510, 0x2500], // ┌─┐ Filler: ─
+#[derive(Debug, PartialEq)]
+pub struct Shield {
+    pub position: Position,
+    pub r#type: ShieldType,
+    owner: Owner,
+    sprite: Sprite,
+}
+
+impl SetSprite for Shield {
+    fn set_sprite(&mut self) {
+        self.sprite = match self.owner {
+            Owner::Player => match self.r#type {
+                ShieldType::None => NO_SPRITE,
+                ShieldType::Single => P_S_SHIELD,
+                ShieldType::Double => P_D_SHIELD,
+                ShieldType::Triple => P_T_SHIELD,
+                ShieldType::Quad => P_Q_SHIELD,
+            },
+            Owner::Invader => match self.r#type {
+                ShieldType::None => NO_SPRITE,
+                ShieldType::Single => I_S_SHIELD,
+                ShieldType::Double => I_D_SHIELD,
+                ShieldType::Triple => I_T_SHIELD,
+                ShieldType::Quad => I_Q_SHIELD,
+            },
+        };
+    }
+}
+
+impl Shield {
+    pub fn new(position: Position, r#type: ShieldType, owner: Owner) -> Shield {
+        let mut shield = Shield {
+            position,
+            r#type,
+            owner,
+            sprite: NO_SPRITE,
+        };
+        shield.set_sprite();
+        shield
+    }
+
+    pub fn upgrade(&mut self) {
+        if self.r#type == ShieldType::Quad {
+            return;
+        }
+        self.r#type = match self.r#type {
+            ShieldType::None => ShieldType::Single,
+            ShieldType::Single => ShieldType::Double,
+            ShieldType::Double => ShieldType::Triple,
+            ShieldType::Triple => ShieldType::Quad,
+            _ => panic!("Out of bounds"),
+        };
+        self.set_sprite();
+    }
+
+    pub fn downgrade(&mut self) {
+        if self.r#type == ShieldType::None {
+            return;
+        }
+        self.r#type = match self.r#type {
+            ShieldType::Single => ShieldType::None,
+            ShieldType::Double => ShieldType::Single,
+            ShieldType::Triple => ShieldType::Double,
+            ShieldType::Quad => ShieldType::Triple,
+            _ => panic!("Out of bounds"),
+        };
+        self.set_sprite();
+    }
+}
+
+pub const P_S_SHIELD: Sprite = [
+    ['\u{0020}', '\u{0020}', '\u{0020}'], //
+    ['\u{250C}', '\u{2500}', '\u{2510}'], // ┌─┐
 ];
 
-pub const P_D_SHIELD: UnicodeMatrix = [
-    [0x20, 0x20, 0x20, 0x20],         //     Filler:
-    [0x2554, 0x2550, 0x2557, 0x2550], // ╔═╗ Filler: ═
+pub const P_D_SHIELD: Sprite = [
+    ['\u{0020}', '\u{0020}', '\u{0020}'], //
+    ['\u{2554}', '\u{2550}', '\u{2557}'], // ╔═╗
 ];
 
-pub const P_T_SHIELD: UnicodeMatrix = [
-    [0x250C, 0x2500, 0x2510, 0x2500], // ┌─┐ Filler: ─
-    [0x2554, 0x2550, 0x2557, 0x2550], // ╔═╗ Filler: ═
+pub const P_T_SHIELD: Sprite = [
+    ['\u{250C}', '\u{2500}', '\u{2510}'], // ┌─┐
+    ['\u{2554}', '\u{2550}', '\u{2557}'], // ╔═╗
 ];
 
-pub const P_Q_SHIELD: UnicodeMatrix = [
-    [0x2554, 0x2550, 0x2557, 0x2550], // ╔═╗ Filler: ═
-    [0x2554, 0x2550, 0x2557, 0x2550], // ╔═╗ Filler: ═
+pub const P_Q_SHIELD: Sprite = [
+    ['\u{2554}', '\u{2550}', '\u{2557}'], // ╔═╗
+    ['\u{2554}', '\u{2550}', '\u{2557}'], // ╔═╗
 ];
 
-pub const I_S_SHIELD: UnicodeMatrix = [
-    [0x2514, 0x2500, 0x2518, 0x2500], // └─┘ Filler: ─
-    [0x20, 0x20, 0x20, 0x20],         //     Filler:
+pub const I_S_SHIELD: Sprite = [
+    ['\u{2514}', '\u{2500}', '\u{2518}'], // └─┘
+    ['\u{0020}', '\u{0020}', '\u{0020}'], //
 ];
 
-pub const I_D_SHIELD: UnicodeMatrix = [
-    [0x255A, 0x2550, 0x255D, 0x2550], // ╚═╝ Filler: ═
-    [0x20, 0x20, 0x20, 0x20],         //     Filler:
+pub const I_D_SHIELD: Sprite = [
+    ['\u{255A}', '\u{2550}', '\u{255D}'], // ╚═╝
+    ['\u{0020}', '\u{0020}', '\u{0020}'], //
 ];
 
-pub const I_T_SHIELD: UnicodeMatrix = [
-    [0x255A, 0x2550, 0x255D, 0x2550], // ╚═╝ Filler: ═
-    [0x2514, 0x2500, 0x2518, 0x2500], // └─┘ Filler: ─
+pub const I_T_SHIELD: Sprite = [
+    ['\u{255A}', '\u{2550}', '\u{255D}'], // ╚═╝
+    ['\u{2514}', '\u{2500}', '\u{2518}'], // └─┘
 ];
 
-pub const I_Q_SHIELD: UnicodeMatrix = [
-    [0x255A, 0x2550, 0x255D, 0x2550], // ╚═╝ Filler: ═
-    [0x255A, 0x2550, 0x255D, 0x2550], // ╚═╝ Filler: ═
+pub const I_Q_SHIELD: Sprite = [
+    ['\u{255A}', '\u{2550}', '\u{255D}'], // ╚═╝
+    ['\u{255A}', '\u{2550}', '\u{255D}'], // ╚═╝
 ];
--- src/game/shield/tests/mod.rs
+++ src/game/shield/tests/mod.rs
@@ -1,7 +1,140 @@
 use super::*;
 
 #[test]
-fn placeholder_test() {
-    let placeholder = "placeholder";
-    assert_eq!(placeholder, "placeholder")
+fn test_new_shield() {
+    let mut shield = Shield::new((0, 0), ShieldType::None, Owner::Player);
+    let mut expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::None,
+        owner: Owner::Player,
+        sprite: NO_SPRITE,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Single, Owner::Player);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Single,
+        owner: Owner::Player,
+        sprite: P_S_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Double, Owner::Player);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Double,
+        owner: Owner::Player,
+        sprite: P_D_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Triple, Owner::Player);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Triple,
+        owner: Owner::Player,
+        sprite: P_T_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Quad, Owner::Player);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Quad,
+        owner: Owner::Player,
+        sprite: P_Q_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::None, Owner::Invader);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::None,
+        owner: Owner::Invader,
+        sprite: NO_SPRITE,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Single, Owner::Invader);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Single,
+        owner: Owner::Invader,
+        sprite: I_S_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Double, Owner::Invader);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Double,
+        owner: Owner::Invader,
+        sprite: I_D_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Triple, Owner::Invader);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Triple,
+        owner: Owner::Invader,
+        sprite: I_T_SHIELD,
+    };
+    assert_eq!(shield, expected);
+
+    shield = Shield::new((0, 0), ShieldType::Quad, Owner::Invader);
+    expected = Shield {
+        position: (0, 0),
+        r#type: ShieldType::Quad,
+        owner: Owner::Invader,
+        sprite: I_Q_SHIELD,
+    };
+    assert_eq!(shield, expected);
+}
+
+#[test]
+fn test_upgrade_shield() {
+    let mut shield = Shield::new((0, 0), ShieldType::None, Owner::Player);
+    shield.upgrade();
+    let mut expected = Shield::new((0, 0), ShieldType::Single, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.upgrade();
+    expected = Shield::new((0, 0), ShieldType::Double, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.upgrade();
+    expected = Shield::new((0, 0), ShieldType::Triple, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.upgrade();
+    expected = Shield::new((0, 0), ShieldType::Quad, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.upgrade();
+    assert_eq!(shield, expected);
+}
+
+#[test]
+fn test_downgrade_shield() {
+    let mut shield = Shield::new((0, 0), ShieldType::Quad, Owner::Player);
+    shield.downgrade();
+    let mut expected = Shield::new((0, 0), ShieldType::Triple, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.downgrade();
+    expected = Shield::new((0, 0), ShieldType::Double, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.downgrade();
+    expected = Shield::new((0, 0), ShieldType::Single, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.downgrade();
+    expected = Shield::new((0, 0), ShieldType::None, Owner::Player);
+    assert_eq!(shield, expected);
+
+    shield.downgrade();
+    assert_eq!(shield, expected);
 }
