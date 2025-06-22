3a91d68cf4d205e561abe4d3d8b9e0c185c77f42
EstebanIbarra <sectorjeat@gmail.com>
2025-06-22 16:30:03 -0600

    Refactors sprite and Owner enum

--- src/game/projectile/mod.rs
+++ src/game/projectile/mod.rs
@@ -1,27 +1,21 @@
 #[cfg(test)]
 mod tests;
 
-use super::Position;
-use crate::sprite::{Sprite, UnicodeMatrix};
+use super::{Owner, Position, Sprite};
 
-pub enum Owner {
-    Player,
-    Invader,
-}
-
-pub struct Projectile<'a> {
+pub struct Projectile {
     pub position: Position,
     pub owner: Owner,
     pub velocity: i16,
-    pub sprite: &'a mut Sprite,
+    pub sprite: Sprite,
 }
 
-pub const P_PROJECTILE: UnicodeMatrix = [
-    [0x20, 0x257F, 0x20, 0x20], // " ╿ " Filler: ' '
-    [0x20, 0x7C, 0x20, 0x20],   // " | " Filler: ' '
+pub const P_PROJECTILE: Sprite = [
+    ['\u{0020}', '\u{257F}', '\u{0020}'], //  ╿
+    ['\u{0020}', '\u{007C}', '\u{0020}'], //  |
 ];
 
-pub const I_PROJECTILE: UnicodeMatrix = [
-    [0x20, 0x7C, 0x20, 0x20],   // " | " Filler: ' '
-    [0x20, 0x257D, 0x20, 0x20], // " ╽ " Filler: ' '
+pub const I_PROJECTILE: Sprite = [
+    ['\u{0020}', '\u{007C}', '\u{0020}'], //  |
+    ['\u{0020}', '\u{257D}', '\u{0020}'], //  ╽
 ];
